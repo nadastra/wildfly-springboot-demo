65d8dc92399516222870947b24a8c8366c7aeefe
nadastra <40881721+nadastra@users.noreply.github.com>
2022-03-04 13:05:56 +1100

    Update SpringbootdemoApplication.java

--- src/main/java/com/example/wildfly/springboot/wildflyspringbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/example/wildfly/springboot/wildflyspringbootdemo/SpringbootdemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
 
 // Spring Boot 2.x
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
