a15fbad44297c929ce4fb577b1213aed3863c53e
nadastra <40881721+nadastra@users.noreply.github.com>
2022-03-21 09:37:59 +1100

    Create SecurityConfig.java

---
+++ src/main/java/com/example/wildfly/springboot/wildflyspringbootdemo/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.wildfly.springboot.wildflyspringbootdemo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+            .and()
+              .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/echo/**")
+                  .hasAuthority("openid")
+                .antMatchers(HttpMethod.POST, "/echo/**")
+                  .hasAuthority("openid")
+                .anyRequest()
+                  .authenticated()
+            .and()
+              .oauth2ResourceServer()
+                .jwt();
+    }
+
+}
