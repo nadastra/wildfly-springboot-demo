a067bfa1752027b7ef70d5076321db885df029d5
nadastra <40881721+nadastra@users.noreply.github.com>
2022-03-21 12:52:54 +1100

    Update SecurityConfig.java

--- src/main/java/com/example/wildfly/springboot/wildflyspringbootdemo/SecurityConfig.java
+++ src/main/java/com/example/wildfly/springboot/wildflyspringbootdemo/SecurityConfig.java
@@ -10,7 +10,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().
+        http.cors()
+            .and()
+            .csrf()
+            .disable()
               .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/echo/**")
                   .hasAuthority("User.Read")
